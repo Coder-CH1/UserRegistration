0f3acd34288f71dd6fb559ad2ae960b4504abd23
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-08-08 12:45:54 +0100

    added

--- routes/login.js
+++ routes/login.js
@@ -1,53 +1,59 @@
-// const Router = require('express').Router();
-// const users = require('../model/users');
-// const {createSecurePassword } = require('../model/users');
-// const {sendEmail} = require('../config/nodemailer');
-// const sqlite = require('../config/db');
+const Router = require('express').Router();
+const users = require('../model/users');
+const {createSecurePassword } = require('../model/users');
+const {sendEmail} = require('../config/nodemailer');
+const sqlite = require('../config/db');
 
-// require('dotenv').config();
+require('dotenv').config();
 
-// const generateOTP = ( ) => {
-//     //always generate 6 digit otp
-//     return Math.floor(100000 + Math.random() * 900000) ;
-// }
-
-
-
-// Router.get('/',(req,res)=>{
-//     res.render('login');
-// });
-
-// Router.post('/',(req,res,next)=>{
-//      let email = req.body.email;
-//      otp = generateOTP();
-
-//      const emailQuery = `
-//      SELECT EMAIL FROM USERS WHERE email = ?
-//      `;
-
-//      sql.query(emailQuery,(err,result)=>{
-//             if ( err ){
-//                 console.log(err);
-//                 res.status(400).json({
-//                     message : "User not Found , Bad Request"
-//                 })
-//             }
-//             else {
-//                sendEmail(email,otp);
-//                console.log(otp);
-//                let otpQuery  = `INSERT INTO OTP VALUES  ('${email' , '${otp}') ON DUPLICATE KEY UPDATE otp = ${otp}`;
-               
-//                sql.query(otpQuery,(err,result)=>{
-//                     if(err)
-//                         console.log(err);
-//                })
-//                console.log('OTP Sent')
-//                res.status(200).redirect('/otp');
-//             }
-//      });
-
-// });
+const generateOTP = ( ) => {
+    //always generate 6 digit otp
+    return Math.floor(100000 + Math.random() * 900000) ;
+}
 
 
 
-// module.exports  = Router;
+Router.get('/',(req,res)=>{
+    res.render('login');
+});
+
+Router.post('/',(req,res,next)=>{
+     const email = req.body.email;
+     const otp = generateOTP();
+
+     const emailQuery = `
+     SELECT EMAIL FROM USERS WHERE email = ?
+     `;
+try {
+    const stmt = sqlite.prepare(emailQuery);
+    const user = stmt.get(email);
+
+    if (!user) {
+        res.status(400).json({
+            message: 'user not found'
+        });
+        return;
+    }
+    sendEmail(email, otp);
+    const otpQuery = `
+        INSERT OR REPLACE INTO OTP (email, otp)
+        VALUES (?, ?)
+    `;
+
+    const otpStmt = sqlite.prepare(otpQuery);
+    otpStmt.run(email, otp);
+
+    console.log('OTP Sent');
+    res.status(200).redirect('/otp');
+
+} catch (err) {
+    console.error(err);
+    res.status(500).json({
+        message: err.message || '',
+    });
+}
+});
+
+
+
+module.exports  = Router;
